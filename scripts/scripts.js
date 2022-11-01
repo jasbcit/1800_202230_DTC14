f1ee6c128d96bf068a1a552e87d3c698fa2adb6e
Cadan Glass <cadanglass@gmail.com>
2022-11-01 13:29:53 -0700

    Add login page with authentication

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+//---------------------------------
+// Your own functions here
+//---------------------------------
+
+function sayHello() {
+  //do something
+}
+//sayHello();    //invoke function
