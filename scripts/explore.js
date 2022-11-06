00c801e7cf2c1ad1dde493caa8fd49cb549bcb74
Cadan Glass <cadanglass@gmail.com>
2022-11-06 00:14:14 -0700

    Replace old explore page with fire store explore

--- scripts/explore.js
+++ scripts/explore.js
@@ -5,36 +5,47 @@ function writeArticles() {
   articlesRef.add({
     code: "A1",
     name: "Make Time to Write: Overcome Your Excuses",
+    details:
+      "By Cynthia ... a full day of work, family and life, you fall into bed ... Mentally ticking...",
 
     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
   });
   articlesRef.add({
     code: "A2",
     name: "Three Proven Ways to Handle Your Writing Anxiety",
+    details: "Discover how to get rid of the writing anxiety once and for all!",
 
     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
   });
   articlesRef.add({
     code: "A3",
     name: "The Myth of the Writing Fairy",
+    details:
+      "Here’s a fun question to ponder. What do The Stand, The Hobbit and A ... Carol all have in common?",
 
     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
   });
   articlesRef.add({
     code: "A4",
     name: "The Benefits of Journal Writing",
+    details:
+      "Everyday we experience many different events. Some good, some bad, some memorable and some that can be forgotten the next day.",
 
     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
   });
   articlesRef.add({
     code: "A5",
     name: "Basic Steps To Plan Academic Dissertation Writing",
+    details:
+      "Let us talk a bit about what SEO is before we get into the SEO article writing guidelines for those that may be new or do not quite understand it.",
 
     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
   });
   articlesRef.add({
     code: "A6",
     name: "Writer’s Web Resources",
+    details:
+      "The Internet has truly ... the careers of writers ... Now you can work for ... ... and a whole range of other clients on a truly global scale. Whether you are in the",
 
     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
   });
@@ -51,13 +62,13 @@ function displayCards(collection) {
         //iterate thru each doc
         var title = doc.data().name; // get value of the "name" key
         var details = doc.data().details; // get value of the "details" key
-        var hikeID = doc.data().code; //get unique ID to each hike to be used for fetching right image
+        var articleID = doc.data().code; //get unique ID to each hike to be used for fetching right image
         let newcard = cardTemplate.content.cloneNode(true);
 
         //update title and text and image
         newcard.querySelector(".card-title").innerHTML = title;
         newcard.querySelector(".card-text").innerHTML = details;
-        newcard.querySelector(".card-image").src = `./images/${hikeID}.jpg`; //Example: NV01.jpg
+        newcard.querySelector(".card-image").src = `./images/explore-images/${articleID}.jpg`; //Example: NV01.jpg
 
         //give unique ids to all elements for future use
         // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
@@ -71,4 +82,4 @@ function displayCards(collection) {
     });
 }
 
-displayCards("hikes");
+displayCards("articles");
