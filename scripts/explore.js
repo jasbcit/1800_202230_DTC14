03ec44c07e1d3c89417be834531de4e595faf6e3
Cadan Glass <cadanglass@gmail.com>
2022-11-05 22:46:09 -0700

    Update article image names for explore page

--- scripts/explore.js
+++ scripts/explore.js
@@ -39,3 +39,36 @@ function writeArticles() {
     last_updated: firebase.firestore.FieldValue.serverTimestamp(),
   });
 }
+
+function displayCards(collection) {
+  let cardTemplate = document.getElementById("hikeCardTemplate");
+
+  db.collection(collection)
+    .get()
+    .then((snap) => {
+      //var i = 1;  //if you want to use commented out section
+      snap.forEach((doc) => {
+        //iterate thru each doc
+        var title = doc.data().name; // get value of the "name" key
+        var details = doc.data().details; // get value of the "details" key
+        var hikeID = doc.data().code; //get unique ID to each hike to be used for fetching right image
+        let newcard = cardTemplate.content.cloneNode(true);
+
+        //update title and text and image
+        newcard.querySelector(".card-title").innerHTML = title;
+        newcard.querySelector(".card-text").innerHTML = details;
+        newcard.querySelector(".card-image").src = `./images/${hikeID}.jpg`; //Example: NV01.jpg
+
+        //give unique ids to all elements for future use
+        // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+        // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+        // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+        //attach to gallery
+        document.getElementById(collection + "-go-here").appendChild(newcard);
+        //i++;   //if you want to use commented out section
+      });
+    });
+}
+
+displayCards("hikes");
