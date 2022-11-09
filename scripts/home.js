dcd6b9329e461d8a0a1a0fd9a076f662ab926fd0
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2022-11-09 13:36:13 -0800

    Add saved text to firebase

---
+++ scripts/home.js
@@ -0,0 +1,24 @@
+
+
+function saveText() {
+  firebase.auth().onAuthStateChanged((user) => {
+    // Check if user is signed in:
+    if (user) {
+      //go to the correct user document by referencing to the user uid
+      currentUser = db.collection("users").doc(user.uid);
+      //get the document for current user.
+      currentUser.get().then((userDoc) => {
+        //get the data fields of the user
+
+        //if the data fields are not empty, then write them in to the form.
+        let userInput = document.querySelector("#user-input").value;
+        db.collection("savedWriting").doc(user.uid).set({
+          text: userInput,
+        });
+      });
+    } else {
+      // No user is signed in.
+      console.log("No user is signed in");
+    }
+  });
+}
