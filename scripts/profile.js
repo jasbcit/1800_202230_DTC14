06a9737a453d5576b0cddfae0ce40bff3e73f932
Jas Randhawa <jrandhawa56@my.bcit.ca>
2022-11-25 11:14:12 -0800

    Save Recently Analyzed to Profile

--- scripts/profile.js
+++ scripts/profile.js
@@ -6,12 +6,19 @@ function insertName() {
     
     if (user) {
         currentUser = db.collection("users").doc(user.uid)
+        currentUserWriting = db.collection("savedWriting").doc(user.uid)
     currentUser.get()
         .then(userDoc =>{
             let userName = userDoc.data().name
             let dateJoined = userDoc.data().datejoined
             userNameDiv.innerText = userName
             dateJoinedDiv.innerText = dateJoined
+        })
+    currentUserWriting.get()
+        .then(userDoc =>{
+          console.log(userDoc.data())
+          let recentlyAnalyzedText = userDoc.data().text
+          document.getElementById("previouslyAnalyzedPlaceholder").innerText = recentlyAnalyzedText
         })
     } else {
       // No user is signed in.
