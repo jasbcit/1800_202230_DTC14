adb4e7d0ac5c52e5635041e3c62bfe50b39d4f89
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2022-12-02 12:46:32 -0800

    Remove commented out code

--- scripts/profile.js
+++ scripts/profile.js
@@ -12,34 +12,7 @@ firebase.auth().onAuthStateChanged(user => {
   }
 });
 
-// function insertName() {
-//   firebase.auth().onAuthStateChanged((user) => {
-//     // Check if a user is signed in:
-//     let userNameDiv = document.querySelector(".profile-user-name");
-//     let dateJoinedDiv = document.querySelector(".profile-date-joined")
 
-//     if (user) {
-//         currentUser = db.collection("users").doc(user.uid)
-//         currentUserWriting = db.collection("savedWriting").doc(user.uid)
-//     currentUser.get()
-//         .then(userDoc =>{
-//             let userName = userDoc.data().name
-//             let dateJoined = userDoc.data().datejoined
-//             userNameDiv.innerText = userName
-//             dateJoinedDiv.innerText = dateJoined
-//         })
-//     currentUserWriting.get()
-//         .then(userDoc =>{
-//           console.log(userDoc.data())
-//           let recentlyAnalyzedText = userDoc.data().text
-//           document.getElementById("previouslyAnalyzedPlaceholder").innerText = recentlyAnalyzedText
-//         })
-//     } else {
-//       // No user is signed in.
-//     }
-//   });
-// }
-// insertName(); //run the function
 
 function insertName() {
   let userNameDiv = document.querySelector(".profile-user-name");
